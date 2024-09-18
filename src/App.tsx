77205a969b252704d8c7ebf1e0af2f3b3ff15184
Bryce <bryce.roy011203@gmail.com>
2024-09-18 17:26:37 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Bryce Roy
+                automatically reload. Bryce Roy the world eater
             </p>
         </div>
     );
