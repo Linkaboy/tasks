913319eaae845d475f1a884ddc1697d3257e0aed
Bryce <bryce.roy011203@gmail.com>
2024-09-19 18:52:03 -0400

    Pushing fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,19 @@ import "./App.css";
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
-            </header>
+            <header className="App-header">This is my header</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.{" "}
                 <span style={{ color: "red" }}>Bryce Roy</span>
-                <Button
-                    onClick={() => {
-                        console.log("Hello World!");
-                    }}
-                >
-                    Log Hello World
-                </Button>
             </p>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
             Making a list:
             <ul>
                 <li>Option A</li>
@@ -34,10 +32,6 @@ function App(): React.JSX.Element {
             <img
                 src="https://t4.ftcdn.net/jpg/07/63/72/23/360_F_763722336_NMoMz3VxW3a5NlezvigHXKMsJPI13GMQ.jpg"
                 alt="Cool Photo"
-            />
-            <img
-                src="../assets/images/pet-ada.jpg"
-                alt="A picture of my dog Ada"
             />
         </div>
     );
